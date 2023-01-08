523c9361c3a0bda2bb6ae43b2361084671e56126
Hasenzahn1 <motzer10@gmx.de>
2023-01-08 12:18:16 +0100

    Some exceptions to give helpfull error messages

---
+++ src/main/java/de/canstein_berlin/customrecipes/exceptions/InvalidRecipeValueException.java
@@ -0,0 +1,9 @@
+package de.canstein_berlin.customrecipes.exceptions;
+
+public class InvalidRecipeValueException extends Exception{
+
+    public InvalidRecipeValueException(String msg){
+        super(msg);
+    }
+}
+
---
+++ src/main/java/de/canstein_berlin/customrecipes/exceptions/MalformedRecipeFileException.java
@@ -0,0 +1,9 @@
+package de.canstein_berlin.customrecipes.exceptions;
+
+public class MalformedRecipeFileException extends Exception{
+
+    public MalformedRecipeFileException(String message){
+        super(message);
+    }
+
+}
