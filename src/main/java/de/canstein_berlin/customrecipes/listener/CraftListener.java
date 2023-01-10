cb996fec04d90949d5ab4a9c00650cfb3c4bc24c
Hasenzahn1 <motzer10@gmx.de>
2023-01-10 22:03:21 +0100

    Added Listener Idea. Not working, will be used later

---
+++ src/main/java/de/canstein_berlin/customrecipes/listener/CraftListener.java
@@ -0,0 +1,14 @@
+package de.canstein_berlin.customrecipes.listener;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+
+import java.util.List;
+
+public class CraftListener implements Listener {
+
+    public void onRecipeCraft(CraftItemEvent event){
+
+    }
+
+}
