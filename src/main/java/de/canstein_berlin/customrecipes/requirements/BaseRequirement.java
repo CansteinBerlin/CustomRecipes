2bd86ba6f6578b06638c16415766feb2053586e7
Hasenzahn1 <motzer10@gmx.de>
2023-01-11 17:20:40 +0100

    Base Requirement class

--- src/main/java/de/canstein_berlin/customrecipes/requirements/BaseRequirement.java
+++ src/main/java/de/canstein_berlin/customrecipes/requirements/BaseRequirement.java
@@ -1,5 +1,22 @@
 package de.canstein_berlin.customrecipes.requirements;
 
-public class BaseRequirement {
+import de.canstein_berlin.customrecipes.exceptions.MalformedRecipeFileException;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.json.JSONObject;
 
+public abstract class BaseRequirement {
+
+    protected final JSONObject jsonObject;
+
+    public BaseRequirement(JSONObject jsonObject) throws MalformedRecipeFileException {
+        this.jsonObject = jsonObject;
+        parse();
+    }
+
+    public abstract boolean check(CraftItemEvent event);
+    protected abstract void parse() throws MalformedRecipeFileException;
+
+    public JSONObject getJsonObject() {
+        return jsonObject;
+    }
 }
