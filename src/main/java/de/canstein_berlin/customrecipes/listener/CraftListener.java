027702968f7c9c477290a91d3bd0a73df8be9fc5
Hasenzahn1 <motzer10@gmx.de>
2023-01-11 17:20:20 +0100

    Listen for the craft event. If one of the required requirements is not met the item cannot be crafted

--- src/main/java/de/canstein_berlin/customrecipes/listener/CraftListener.java
+++ src/main/java/de/canstein_berlin/customrecipes/listener/CraftListener.java
@@ -1,14 +1,26 @@
 package de.canstein_berlin.customrecipes.listener;
 
+import de.canstein_berlin.customrecipes.CustomRecipes;
+import de.canstein_berlin.customrecipes.requirements.BaseRequirement;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.Recipe;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class CraftListener implements Listener {
 
+    @EventHandler
     public void onRecipeCraft(CraftItemEvent event){
-
+        Recipe recipe = event.getRecipe();
+        ArrayList<BaseRequirement> requirements = CustomRecipes.getInstance().getRequirements(recipe);
+        for(BaseRequirement r : requirements){
+            if(!r.check(event)){
+                event.setCancelled(true);
+                return;
+            }
+        }
     }
-
 }
