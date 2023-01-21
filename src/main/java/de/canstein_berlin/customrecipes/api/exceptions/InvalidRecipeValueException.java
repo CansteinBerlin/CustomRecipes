d53bd339cc62b346b18d2d69d43632a4cddb0ef9
Hasenzahn1 <motzer10@gmx.de>
2023-01-21 20:01:15 +0100

    Exceptions

---
+++ src/main/java/de/canstein_berlin/customrecipes/api/exceptions/InvalidRecipeValueException.java
@@ -0,0 +1,9 @@
+package de.canstein_berlin.customrecipes.api.exceptions;
+
+public class InvalidRecipeValueException extends Exception{
+
+    public InvalidRecipeValueException(String msg){
+        super(msg);
+    }
+}
+
---
+++ src/main/java/de/canstein_berlin/customrecipes/api/exceptions/MalformedRecipeFileException.java
@@ -0,0 +1,9 @@
+package de.canstein_berlin.customrecipes.api.exceptions;
+
+public class MalformedRecipeFileException extends Exception{
+
+    public MalformedRecipeFileException(String message){
+        super(message);
+    }
+
+}
