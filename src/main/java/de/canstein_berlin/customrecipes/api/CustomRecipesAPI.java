1addd8c80e5a8f005824596c8106f9638f470a59
Hasenzahn1 <motzer10@gmx.de>
2023-01-22 11:21:35 +0100

    Api now provides methods to create and register Recipes at the same time.

--- src/main/java/de/canstein_berlin/customrecipes/api/CustomRecipesAPI.java
+++ src/main/java/de/canstein_berlin/customrecipes/api/CustomRecipesAPI.java
@@ -5,37 +5,41 @@ import de.canstein_berlin.customrecipes.api.recipes.parser.BaseRecipeParser;
 import de.canstein_berlin.customrecipes.api.recipes.parser.RecipeParserFactory;
 import de.canstein_berlin.customrecipes.api.recipes.parser.type.*;
 import de.canstein_berlin.customrecipes.api.requirements.BaseRequirement;
+import de.canstein_berlin.customrecipes.api.requirements.type.GamemodeRequirement;
+import de.canstein_berlin.customrecipes.api.requirements.type.PermissionRequirement;
 import org.bukkit.Bukkit;
-import org.bukkit.inventory.Recipe;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.*;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
+import java.io.File;
 import java.util.HashMap;
 
 public class CustomRecipesAPI {
 
     private static CustomRecipesAPI instance;
 
-    private final HashMap<String, BaseRequirement> requirements;
-    private final ArrayList<CustomRecipe> recipesWithRequirements;
+    private final HashMap<NamespacedKey, CustomRecipe> recipesWithRequirements;
 
     private CustomRecipesAPI() {
         instance = this;
 
-        requirements = new HashMap<>();
-        recipesWithRequirements = new ArrayList<>();
+        recipesWithRequirements = new HashMap<>();
 
         //Register parser
-        RecipeParserFactory.getInstance().addParser(new CraftingShapedRecipeParser());
-        RecipeParserFactory.getInstance().addParser(new CraftingShapelessRecipeParser());
-        RecipeParserFactory.getInstance().addParser(new SmeltingRecipeParser());
-        RecipeParserFactory.getInstance().addParser(new BlastingRecipeParser());
-        RecipeParserFactory.getInstance().addParser(new SmokingRecipeParser());
-        RecipeParserFactory.getInstance().addParser(new CampfireCookingRecipeParser());
-        RecipeParserFactory.getInstance().addParser(new StonecuttingRecipeParser());
-        RecipeParserFactory.getInstance().addParser(new SmithingRecipeParser());
+        registerParser(new CraftingShapedRecipeParser());
+        registerParser(new CraftingShapelessRecipeParser());
+        registerParser(new SmeltingRecipeParser());
+        registerParser(new BlastingRecipeParser());
+        registerParser(new SmokingRecipeParser());
+        registerParser(new CampfireCookingRecipeParser());
+        registerParser(new StonecuttingRecipeParser());
+        registerParser(new SmithingRecipeParser());
 
         //Register Requirements
-
+        registerNewRequirement(new PermissionRequirement());
+        registerNewRequirement(new GamemodeRequirement());
     }
 
     public static CustomRecipesAPI getInstance() {
@@ -43,21 +47,48 @@ public class CustomRecipesAPI {
         return instance;
     }
 
-    public void registerRecipes(CustomRecipe recipe) {
-        Bukkit.removeRecipe(recipe.getNamespacedKey());
-        Bukkit.addRecipe(recipe.getRecipe());
+    public static NamespacedKey getNamespacedKeyFromRecipe(Recipe recipe) {
+        if (recipe instanceof ShapedRecipe) return ((ShapedRecipe) recipe).getKey();
+        if (recipe instanceof ShapelessRecipe) return ((ShapelessRecipe) recipe).getKey();
+        if (recipe instanceof CookingRecipe) return ((CookingRecipe<?>) recipe).getKey();
+        if (recipe instanceof SmithingRecipe) return ((SmithingRecipe) recipe).getKey();
+        if (recipe instanceof StonecuttingRecipe) return ((StonecuttingRecipe) recipe).getKey();
+        return null;
     }
 
-    public void registerRecipes(Recipe recipe) {
+    public boolean createAndRegister(JavaPlugin plugin, String file) {
+        return createAndRegister(plugin, new File(plugin.getDataFolder(), file));
+    }
 
+    public boolean createAndRegister(JavaPlugin plugin, File file) {
+        CustomRecipe recipe = CustomRecipe.fromResource(plugin, file);
+        if (recipe == null) return false;
+        return registerRecipes(recipe);
+    }
+
+    public boolean registerRecipes(CustomRecipe recipe) {
+        unregisterRecipe(recipe);
+
+        boolean value = Bukkit.addRecipe(recipe.getRecipe());
+        if (recipe.hasRequirements()) {
+            recipesWithRequirements.put(recipe.getNamespacedKey(), recipe);
+        }
+        return value;
+    }
+
+    public boolean registerRecipes(Recipe recipe) {
+        unregisterRecipe(recipe);
+
+        return Bukkit.addRecipe(recipe);
     }
 
     public void unregisterRecipe(CustomRecipe recipe) {
-
+        Bukkit.removeRecipe(recipe.getNamespacedKey());
+        recipesWithRequirements.remove(recipe.getNamespacedKey());
     }
 
     public void unregisterRecipe(Recipe recipe) {
-
+        Bukkit.removeRecipe(getNamespacedKeyFromRecipe(recipe));
     }
 
     public void registerParser(BaseRecipeParser recipeParser) {
@@ -65,10 +96,14 @@ public class CustomRecipesAPI {
     }
 
     public void registerNewRequirement(BaseRequirement requirement) {
-        requirements.put(requirement.getId(), requirement);
+        RecipeParserFactory.getInstance().addRequirement(requirement);
     }
 
-    public HashMap<String, BaseRequirement> getRequirements() {
-        return requirements;
+    public boolean canCraftRecipe(Recipe recipe, CraftItemEvent event) {
+        NamespacedKey key = getNamespacedKeyFromRecipe(recipe);
+        if (recipesWithRequirements.containsKey(key)) {
+            return recipesWithRequirements.get(key).canCraft(event);
+        }
+        return true;
     }
 }
