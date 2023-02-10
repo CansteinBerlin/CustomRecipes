cae61661976f7df1d2ef2985b8d11cbdec2a9437
Hasenzahn1 <motzer10@gmx.de>
2023-02-10 18:03:24 +0100

    ListRecipes Command now can use an argument to specify which recipe should be displayed. This also allows the display of the minecraft recipes. Also Tabcompletion

--- src/main/java/de/canstein_berlin/customrecipes/commands/ListRecipesCommand.java
+++ src/main/java/de/canstein_berlin/customrecipes/commands/ListRecipesCommand.java
@@ -2,17 +2,28 @@ package de.canstein_berlin.customrecipes.commands;
 
 import de.canstein_berlin.customrecipes.CustomRecipes;
 import de.canstein_berlin.customrecipes.api.CustomRecipesAPI;
+import de.canstein_berlin.customrecipes.api.recipes.CustomRecipe;
 import de.canstein_berlin.customrecipes.gui.ListAllRecipesGUI;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Recipe;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Permission: customrecipes.commands.listallrecipesgui
  */
-public class ListRecipesCommand implements CommandExecutor {
+public class ListRecipesCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -28,10 +39,51 @@ public class ListRecipesCommand implements CommandExecutor {
             return true;
         }
 
-        new ListAllRecipesGUI(CustomRecipesAPI.getInstance().getDefinedRecipes()).show(((Player) sender));
-        sender.sendMessage(CustomRecipes.getLang("lang.commands.listAllRecipes.success"));
+        Player p = ((Player) sender);
 
+        if (args.length == 0) args = new String[]{"all"};
+
+        if (args[0].equalsIgnoreCase("all")) {
+            System.out.println("get All recipes");
+            showGUI(p, CustomRecipesAPI.getInstance().getDefinedRecipes(""));
+        } else if (args[0].equalsIgnoreCase("minecraft")) {
+            System.out.println("get Minecraft Recipe");
+            showGUI(p, getMinecraftRecipes());
+        } else if (CustomRecipesAPI.getInstance().getDefinedRecipes(args[0]).size() != 0) {
+            System.out.println("get specific Recipe");
+            showGUI(p, CustomRecipesAPI.getInstance().getDefinedRecipes(args[0]));
+        } else {
+            sender.sendMessage(CustomRecipes.getLang("lang.commands.listAllRecipes.noNamespace", "namespace", args[0]));
+        }
         return true;
     }
 
+    private void showGUI(Player sender, ArrayList<CustomRecipe> recipes) {
+        new ListAllRecipesGUI(recipes).show(sender);
+        sender.sendMessage(CustomRecipes.getLang("lang.commands.listAllRecipes.success"));
+    }
+
+    private ArrayList<CustomRecipe> getMinecraftRecipes() {
+        ArrayList<CustomRecipe> recipes = new ArrayList<>();
+        Iterator<Recipe> recipeIterator = Bukkit.getServer().recipeIterator();
+        while (recipeIterator.hasNext()) {
+            Recipe recipe = recipeIterator.next();
+            NamespacedKey key = CustomRecipesAPI.getNamespacedKeyFromRecipe(recipe);
+            if (key == null) continue;
+            recipes.add(new CustomRecipe(key, recipe));
+        }
+        return recipes;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        List<String> possible = new ArrayList<>(List.of("minecraft"));
+        possible.addAll(CustomRecipesAPI.getInstance().getRegisteredNamespaces());
+
+        return possible
+                .stream()
+                .filter(s -> s.startsWith(args[0]))
+                .sorted()
+                .collect(Collectors.toList());
+    }
 }
